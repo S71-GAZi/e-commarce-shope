aea846065d937dea9d6d8b229589f18c9459d93e
Sorifullah Pavel <sorifullah.pavel@savethechildren.org>
2026-03-01 10:31:18 +0600

    Add category image and add daynamic in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic"
+
 import { Header } from "@/components/layout/header"
 import { Footer } from "@/components/layout/footer"
 import { HeroBanner } from "@/components/home/hero-banner"
